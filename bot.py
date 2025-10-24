2afbff64710c345f2e0ad5ddc37f6a7492a382f1
aryanjuyal <aryanjuyal70@gmail.com>
2025-10-24 16:49:33 +0530

    Implement Gemini API integration in bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+from google import genai
+from PIL import Image
+from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key=os.getenv("GEMINI_API_KEY")
+client = genai.Client()
+
+def gemini(prompt,image=None):
+   contents = [prompt]
+   if image:
+        img = Image.open(image)
+        contents.append(img)
+  
+   response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=contents
+        
+       
+    )
+   return response.text
+
